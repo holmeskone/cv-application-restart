d798b0f2cdbd4280516afb642bfe38315f7091b1
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-30 15:20:13 +0100

    Add ability to input value and display text on other side of the page, now i need to find a way to make this easy to do and not to have 10+ functions for it

--- CVApp/src/App.jsx
+++ CVApp/src/App.jsx
@@ -1,34 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Input from "./input"
+import Display from "./display"
+import { useState } from "react";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [firstName, setfirstName] = useState("John")
+  // const [lastName, setlasttName] = useState("Doe")
 
+  const handleInput = (event) =>{
+    const input = event.target.value;
+    console.log(input)
+    // console.log(`This should be for ${id}`)
+    return(
+      setfirstName(input)
+    )
+  }
   return (
-    <>
+    <div className="container">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" onChange={handleInput} />
+        <input type="text" onChange={handleInput} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <Display
+        text ={firstName}  />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
