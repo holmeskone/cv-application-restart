ad1d5b40876702c0947c296dd9e477113a742582
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-31 10:23:56 +0100

    Import input fields, create states for each field, send setState to each input field and send the state value to the display component to show in the output-section

--- CVApp/src/App.jsx
+++ CVApp/src/App.jsx
@@ -1,29 +1,37 @@
-import Input from "./input"
+import InputFirstName from "./components/FirstName"
+import InputLastName from "./components/LastName";
+import InputEmail from "./components/Email";
+import InputPhone from "./components/Phone";
+import InputNationality from "./components/Nationality";
 import Display from "./display"
 import { useState } from "react";
 import './App.css';
 
 function App() {
   const [firstName, setfirstName] = useState("John")
-  // const [lastName, setlasttName] = useState("Doe")
+  const [lastName, setlastName] = useState("Doe")
+  const [email, setEmail] = useState("example@example.com")
+  const [phone, setPhone] = useState("+123456789")
+  const [nationality, setNationality] = useState("EU Citizen")
 
-  const handleInput = (event) =>{
-    const input = event.target.value;
-    console.log(input)
-    // console.log(`This should be for ${id}`)
-    return(
-      setfirstName(input)
-    )
-  }
   return (
     <div className="container">
-      <div>
-        <input type="text" onChange={handleInput} />
-        <input type="text" onChange={handleInput} />
+      <div id="input-section">
+        <div id="personal-information">
+          <h3>Personal Information Section</h3>
+          <InputFirstName id="first-name" label = "First Name" value ={firstName} setValue={setfirstName}/>
+          <InputLastName id="last-name" label = "Last Name" value ={lastName} setValue={setlastName} />
+          <InputEmail id="email" label = "Email" value ={email} setValue={setEmail} />
+          <InputPhone id="phone" label = "Phone" value ={phone} setValue={setPhone} />
+          <InputNationality id="nationality" label = "Nationality" value ={nationality} setValue={setNationality} />
+        </div>
       </div>
-      <div>
-        <Display
-        text ={firstName}  />
+      <div id="output-section">
+        <Display text={firstName} />
+        <Display text={lastName}/>
+        <Display text={email} />
+        <Display text={phone}/>
+        <Display text={nationality}/>
       </div>
     </div>
   )
