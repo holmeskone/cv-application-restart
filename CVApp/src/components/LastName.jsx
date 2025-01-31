eff856827d2b8c41efacbf674637e647f92db3da
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-31 10:12:56 +0100

    Idem to fist name

---
+++ CVApp/src/components/LastName.jsx
@@ -0,0 +1,10 @@
+const InputLastName = props => {
+    return (
+      <div id={props.id}>
+      <label htmlFor="">{props.label} </label>
+     <input  onChange={(event) => props.setValue(event.target.value)}></input>
+     </div>
+   )
+  }
+
+export default InputLastName
