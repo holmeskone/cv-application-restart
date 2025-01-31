c122551c4fc6ef13b712cb40c0cfd8cfbb90998c
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-01-31 10:24:35 +0100

    Idem all other input fields

---
+++ CVApp/src/components/Nationality.jsx
@@ -0,0 +1,10 @@
+const InputNationality = props => {
+    return (
+      <div id={props.id}>
+      <label htmlFor="">{props.label} </label>
+     <input  onChange={(event) => props.setValue(event.target.value)}></input>
+     </div>
+   )
+  }
+
+export default InputNationality
