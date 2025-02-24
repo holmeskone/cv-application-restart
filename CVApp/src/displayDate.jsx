756144410c049dc443ee022aead16a8a2834495d
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-02-24 10:14:57 +0100

    Create function to display dates for CV

---
+++ CVApp/src/displayDate.jsx
@@ -0,0 +1,11 @@
+function DisplayDate (prop) {
+    let month = prop.date.toLocaleString('default', { month: 'long' });
+    let year = prop.date.getFullYear()
+    return(
+        <div>
+        <p>{month} {year}</p>
+      </div>
+    )
+}
+
+export default DisplayDate
