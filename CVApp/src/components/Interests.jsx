02c0de53c24c640a849c8d5ee3880e7edefcdf31
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-02-27 11:52:25 +0100

    Mimic Skills.jsx and Languages.jsx section to add interests

---
+++ CVApp/src/components/Interests.jsx
@@ -0,0 +1,40 @@
+function DynamicInterests(prop) {
+    const handleAddInput = () => {
+        prop.setInputs([...prop.inputs, {interest:""}]);
+    };
+
+    const handleChange = (event,index) =>{
+        let {name, value} = event.target;
+        let onChangeValue = [...prop.inputs];
+        onChangeValue[index][name] = value;
+        prop.setInputs(onChangeValue)
+    }
+
+    const handleDeleteInput = (index) => {
+        const newArray = [...prop.inputs];
+        newArray.splice(index, 1);
+        prop.setInputs(newArray);
+      };
+return(
+    <div className="dynamic-interests">
+        {prop.inputs.map((item, index) => (
+            <div className="input-skill" key={index}>
+                <input 
+                    type="text"
+                    name="interest"
+                    value={item.interest}
+                    onChange={(event) => handleChange(event, index)}
+                />
+                {prop.inputs.length > 1 && (
+                <button onClick={() => handleDeleteInput(index)}>Delete Interest</button>
+                )}
+                {index === prop.inputs.length - 1 &&(
+                    <button onClick={() => handleAddInput(index)}>Add Interest</button>
+                )}
+            </div>
+        ))}
+    </div>
+)
+}
+
+export default DynamicInterests
