61d8536935e78719ba5d2bbbcaccd18aac3244cc
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-02-27 22:52:03 +0100

    Add value as prop and include as classname of children for styling purposes

--- CVApp/src/components/Accordion.jsx
+++ CVApp/src/components/Accordion.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react"
-function Accordion ({title, children}){
+function Accordion ({title, value, children}){
     const [isActive, setActive] = useState(false);
     return(
         <div className="accordion">
             <div className="accordion-item">
             <div className="accordion-title" onClick={() => setActive(!isActive)}>
-                <div>
+                <div className="accordion-title-content">
                     <h3>{title}</h3>
                     <div>{isActive ? '-' : '+'}</div>
                 </div>
-                </div>{isActive && <div className="accordion-content">{children}</div>}
+                </div>{isActive && <div className="accordion-content"><div className={value}>{children}</div></div>}
             </div>
         </div>
     )
