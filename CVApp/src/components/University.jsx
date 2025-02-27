6cd75d311fe7d428432a7039192e556750e2e7de
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-02-27 11:03:48 +0100

    Add layout to display input for university inputs

---
+++ CVApp/src/components/University.jsx
@@ -0,0 +1,10 @@
+const InputUniverisity = (props) => {
+    return (
+      <div id={props.id}>
+      <label htmlFor="">{props.label} </label>
+     <input  onChange={(event) => props.setValue(event.target.value)}></input>
+     </div>
+   )
+  }
+
+export default InputUniverisity
