8fd076319388f7082f246681036ba40e60055e64
holmeskone <165698602+holmeskone@users.noreply.github.com>
2025-02-27 11:42:02 +0100

    Same format as displaying a language but for skills

---
+++ CVApp/src/components/Skills.jsx
@@ -0,0 +1,40 @@
+function DynamicSkills(prop) {
+    const handleAddInput = () => {
+        prop.setInputs([...prop.inputs, {skill:""}]);
+    };
+
+    const handleChange = (event,index) =>{
+        let {name, value} = event.target;
+        let onChangeValue = [...prop.inputs];
+        onChangeValue[index][name] = value;
+        prop.setInputs(onChangeValue)
+    }
+
+    const handleDeleteInput = (index) => {
+        const newArray = [...prop.inputs];
+        newArray.splice(index, 1);
+        prop.setInputs(newArray);
+      };
+return(
+    <div className="dynamic-skills">
+        {prop.inputs.map((item, index) => (
+            <div className="input-skill" key={index}>
+                <input 
+                    type="text"
+                    name="skill"
+                    value={item.skill}
+                    onChange={(event) => handleChange(event, index)}
+                />
+                {prop.inputs.length > 1 && (
+                <button onClick={() => handleDeleteInput(index)}>Delete Skill</button>
+                )}
+                {index === prop.inputs.length - 1 &&(
+                    <button onClick={() => handleAddInput(index)}>Add Skill</button>
+                )}
+            </div>
+        ))}
+    </div>
+)
+}
+
+export default DynamicSkills
